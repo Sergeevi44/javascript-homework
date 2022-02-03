839456b5329f99ead9106014c4c8e1099d1f2875
Oleksii <sergeevi44@gmail.com>
2022-02-03 18:40:54 +0200

    hw07-done

--- homework-07/src/js/task-08.js
+++ homework-07/src/js/task-08.js
@@ -4,17 +4,19 @@ const refs = {
 	clearBtnEl: document.querySelector('[data-action="destroy"]'),
 	resultBoxesEl: document.querySelector('#boxes'),
 }
-
 const createBoxes = amount => {
 	amount = refs.inputEl.value;
-	refs.resultBoxesEl.setAttribute('style', 'display:flex');
+	refs.resultBoxesEl.setAttribute('style', 'display:flex; justify-content:space-between; align-items:center; flex-wrap:wrap');
 	for (let i = 0; i < amount; i+=1) {
-		refs.resultBoxesEl.insertAdjacentHTML('beforeend', `<div class="box" style="background-color:teal" width=${30 + i * 10} height = ${30 + i * 10}></div>`);
-
+		refs.resultBoxesEl.insertAdjacentHTML('beforeend', `<div class="box" style="background-color:rgb(${(Math.round(Math.random()*255)).toString()},${(Math.round(Math.random()*255)).toString()},${(Math.round(Math.random()*255)).toString()}); width:${30 + i * 10}px; height:${30 + i * 10}px"></div>`);
 	}
+	refs.renderBtnEl.setAttribute('disabled', 'true');
 }
-const destroyBoxes=()=> {
-	
+const destroyBoxes = () => {
+	refs.resultBoxesEl.innerHTML = '';
+	refs.inputEl.value = 0;
+	refs.renderBtnEl.removeAttribute('disabled');
 }
 
 refs.renderBtnEl.addEventListener('click', createBoxes);
+refs.clearBtnEl.addEventListener('click', destroyBoxes);
